53682aa9192019a257cdf0985f795abc1e8e4d4d
Francisco <bentonetofr@gmail.com>
2026-05-02 23:47:55 -0300

    corrige erro no supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,7 @@
 const SUPABASE_URL = "https://yybcfzwrjdjgybagjcgm.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5YmNmendyamRqZ3liYWdqY2dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NTM3MzksImV4cCI6MjA5MzMyOTczOX0.yBYSGoBgbCOJR0hDKr2lsY7f12amhbjm1qd36pkK4cE";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
